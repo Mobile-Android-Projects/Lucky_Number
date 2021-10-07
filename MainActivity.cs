3f232f430ec778eb8b1bcd5d539aec2eb5a12f4f
Auravision <auravision.africa@gmail.com>
2021-10-07 08:18:36 +0300

    Linked design to Code. App done

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,20 +1,41 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using System;
 
 namespace LuckyNumber
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        SeekBar seekBar;
+        Button rollBtn;
+        TextView resultTxtView;
+
+        Random random;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            rollBtn = FindViewById<Button>(Resource.Id.rollButton);
+            resultTxtView = FindViewById<TextView>(Resource.Id.resultTextView);
+
+            //create instance of random number generator
+            random = new Random();
+
+            rollBtn.Click += RollBtn_Click;
         }
-      
+
+        private void RollBtn_Click(object sender, System.EventArgs e)
+        {
+            int luckyNumber = random.Next(seekBar.Progress) + 1;
+            resultTxtView.Text = luckyNumber.ToString();
+        }
     }
 }
